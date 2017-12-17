a739762de6aeffd4c30c63cec051b9793415bc8a
jason <aeq@qq.com>
2017-12-18 00:27:54 +0800

    menu组件开发

--- src/components/content/leftpart.vue
+++ src/components/content/leftpart.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="subNavBox">
-    <menu_item></menu_item>
+    <div class="sBox"  v-for="(item,index) in p_menu_list" :key="item.id">
+      <div class="subNav " v-bind:class="item.isopen?'sublist-down':'sublist-up'"  v-on:click="dohover(index)">
+        <span class="title-icon glyphicon " v-bind:class="item.isopen?'glyphicon-chevron-down':'glyphicon-chevron-up'"></span><span
+        class="sublist-title">{{item.name}}</span>
+      </div>
+      <s_menu_list v-bind:childList="item.sunList"></s_menu_list>
+    </div>
+    <!-- <menu_item v-for="(item,index) in p_menu_list" :key="item.id"
+     v-bind:childList="item.childList"
+     >
+     </menu_item>-->
   </div>
 </template>
 <script>
-  import menu_item from "./left/menu_item";
+  import s_menu_list from "./left/s_menu_list";
 
   export default {
-    components: {menu_item},
+    components: {s_menu_list},
     name: "leftpart",
     data: function () {
-      return {}
+      return {
+        p_menu_list: []
+      }
     }, created: function () {
 
+      var ment_list_url = server_host + "upms/menu/list";
+      this.$http.get(ment_list_url, {}, {
+        headers: {},
+        emulateJSON: true
+      }).then((response) => {
+
+        this.p_menu_list = response.data;
+        // 响应成功回调
+      }).catch((e) => {
+        debugger
+      });
     },
-    methods: {}
+    methods: {
+      dohover:function (index) {
+        this.p_menu_list[index].isopen=!this.p_menu_list[index].isopen;
+      }
+    }
   }
 </script>
 <style scoped>
