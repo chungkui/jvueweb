7b2bd090c16adc5b01d92476c432787d63481307
jason <zhang_s@centit.com>
2017-12-25 17:36:32 +0800

    动态加载组件

--- src/components/content/leftpart.vue
+++ src/components/content/leftpart.vue
@@ -21,7 +21,7 @@
 </template>
 <script>
   import s_menu_item from "./left/s_menu_item";
-
+  import lasyLoading from '../../util/lazyLoading'
   export default {
     components: {s_menu_item},
     name: "leftpart",
@@ -34,9 +34,9 @@
     }, created: function () {
       var ment_list_url = server_host + "upms/permission/list?permissionId=1";
       this.$http.get(ment_list_url, {params: {}, credentials: true}).then((response) => {
-
         this.p_menu_list = response.data;
-
+        /*这里需要动态的加载路由*/
+       this.paserPermission2router()
         // 响应成功回调
       }).catch((e) => {
 
@@ -51,7 +51,31 @@
         } else {
           this.pselectedId = index;
         }
+      },
+      paserPermission2router:function () {
+        debugger;
+        var munulist=this.p_menu_list;
+        let routerlist=[{
+          path: '/',
+          component: Main,
+          children: []
+        }];
+        for(let menu in munulist){
+          if(menu.sunList.length>0){
+            for(let sun in menu.sunList){
+              lasyLoading(sun.name);
+              let routeritem= {
+                path: sun.router_uri,
+                name: sun.name,
+                component: sun.name,
+                props: true
+              };
+              routeritem.push(routeritem);
+            }
+          }
+        }
 
+        this.$router.addRoutes(routerlist);
       }
     }
   }
